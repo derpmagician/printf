892547b3d929fadaa8cc9aab15be00367eabe90a
Renzo <renzin@gmail.com>
2021-03-12 21:13:01 +0000

    adding v1 of the headers

---
+++ holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdarg.h>
+
+/**
+  * struct specifiers - Struct specifiers
+  * @specifier: The conversion specifier
+  * @f: The function pointer
+  */
+
+int _printf(const char *format, ...);
+
+#endif /* _HOLBERTON */
