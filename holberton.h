3b53d23936bbb02c949ae0bf5676e0f77427b184
joseheco <jferhc@gmail.com>
2021-03-13 18:54:45 -0500

    Print_reverse

--- holberton.h
+++ holberton.h
@@ -20,6 +20,7 @@ int _print_a_string(va_list args);
 int _print_format(const char *format, va_list args);
 int _print_spec(char format, va_list args);
 int _print_int_binary(va_list args);
+int _print_reverse(va_list arg);
 void _recursion_int_binary(int a);
 
 #endif /* _HOLBERTON */
