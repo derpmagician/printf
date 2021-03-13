33314888f2fe541e3ff79862462ff398cbe6b9a8
Renzo <renzin@gmail.com>
2021-03-13 23:18:23 +0000

    adding specifier validator

--- print_format.c
+++ print_format.c
@@ -47,3 +47,34 @@ int _print_format(const char *format, va_list args)
 
 	return (count);
 }
+
+/**
+  * _print_spec - Prints a valid specifier
+  * @format: The specifier to prints
+  * @args: A list of variadic arguments
+  *
+  * Return: The length of the specifier
+  */
+int _print_spec(char format, va_list args)
+{
+	int i  = 0, length = 0;
+	spc_dt _types[] = {
+		{"c", _print_a_char},
+		{"s", _print_a_string},
+		{"d", _print_a_integer},
+		{"i", _print_a_integer},
+		{"b", _print_int_binary},
+		{NULL, NULL}
+	};
+
+	while (_types[i].specifier)
+	{
+		if (*_types[i].specifier == format)
+			length = _types[i].f(args);
+
+		i++;
+	}
+
+	return (length);
+}
+
