7ce84f7daac85faaab26e1ee7ff1ba6d9c1e485b
joseheco <jferhc@gmail.com>
2021-03-14 16:17:47 -0500

    Print_address

--- print_address.c
+++ print_address.c
@@ -9,12 +9,11 @@
 
 int _print_address(va_list arg)
 {
-    char *str = va_arg(arg, char *);
-    int i = 0;
-
-    if (str == NULL)
-    str = "(null)";
-    for (; str[i]; i++)
-    _putchar(str[i]);
-    return (i);
+char *str = va_arg(arg, char *);
+int i = 0;
+if (str == NULL)
+str = "(null)";
+for (; str[i]; i++)
+_putchar(str[i]);
+return (i);
 }
