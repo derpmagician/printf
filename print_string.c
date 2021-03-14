e194d3951d9f0ce74ebb2dcabc263e77d48c0459
joseheco <jferhc@gmail.com>
2021-03-14 16:21:02 -0500

    Print_String

--- print_string.c
+++ print_string.c
@@ -10,12 +10,11 @@
 int _print_String(va_list arg)
 {
 
-    char *str = va_arg(arg, char *);
-    int i = 0;
-
-        if (str == NULL)
-            str = "(null)";
-        for (; str[i]; i++)
-            _putchar(str[i]);
-        return (i);
+char *str = va_arg(arg, char *);
+int i = 0;
+if (str == NULL)
+str = "(null)";
+for (; str[i]; i++)
+_putchar(str[i]);
+return (i);
 }
