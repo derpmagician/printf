eaf7b1c7f80b58a5690123799a32ed024d3ea26e
Renzo <renzin@gmail.com>
2021-03-14 00:41:43 +0000

    adding validators

--- print_format.c
+++ print_format.c
@@ -78,3 +78,47 @@ int _print_spec(char format, va_list args)
 	return (length);
 }
 
+/**
+  * _print_invalid_spec - Prints a invalid specifier
+  * @prev_format: The previous specifier of actual specifier
+  * @format: The specifier to prints
+  * @count: The current count before prints invalid specifiers
+  *
+  * Return: The current count after prints invalid specifiers
+  */
+int _print_invalid_spec(char prev_format, char format, int count)
+{
+	count += _write('%');
+
+	if (prev_format == ' ')
+	{
+		count += _write(' ');
+		count += _write(format);
+	}
+	else
+	{
+		count += _write(format);
+	}
+
+	return (count);
+}
+
+/**
+  * _validate_char - validate the type
+  * @_type: character to be comparate
+  *
+  * Return: 1 if char is equal to a type
+  */
+int _validate_char(char _type)
+{
+	char _types[] = {'c', 's', 'd', 'i', 'b', '%'};
+	int i = 0;
+
+	while (_types[i])
+	{
+		if (_types[i] == _type)
+			return (1);
+		i++;
+	}
+	return (0);
+}
