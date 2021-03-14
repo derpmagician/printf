21c993b61d9dee296a34fb135dfae17841a4e334
joseheco <jferhc@gmail.com>
2021-03-14 16:15:29 -0500

    Print_rot13

--- print_address.c
+++ print_address.c
@@ -8,4 +8,13 @@
  */
 
 int _print_address(va_list arg)
+{
+    char *str = va_arg(arg, char *);
+    int i = 0;
 
+    if (str == NULL)
+    str = "(null)";
+    for (; str[i]; i++)
+    _putchar(str[i]);
+    return (i);
+}
--- print_rot13.c
+++ print_rot13.c
@@ -10,31 +10,30 @@
 
 int _print_rot(va_list arg)
 {
+int i, j, count = 0;
+char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+char *s = va_arg(arg, char *);
 
-    int i, j, count = 0;
-    char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-    char *s = va_arg(arg, char *);
-
-    if (s == NULL)
-        s = "(NULL)";
-        for (i = 0; s[i]; i++)
-        {
-            for (j = 0; j < 52; j++)
-            {
-                if (s[i] == a[j])
-                    break;
-            }
-                if (s[i] == a[j])
-                {
-                    _putchar(b[j]);
-                        count++;
-                }
-                else
-                {   
-                    _putchar(s[i]);
-                        count++;
-                }
-        }
-    return (count);
+if (s == NULL)
+s = "(NULL)";
+for (i = 0; s[i]; i++)
+{
+for (j = 0; j < 52; j++)
+{
+if (s[i] == a[j])
+break;
+}
+if (s[i] == a[j])
+{
+_putchar(b[j]);
+count++;
+}
+else
+{
+_putchar(s[i]);
+count++;
+}
+}
+return (count);
 }
