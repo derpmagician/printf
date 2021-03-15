c73e7e6680cad9fe8b32096042240ce907bf38a4
Renzo <renzin@gmail.com>
2021-03-15 20:57:25 +0000

    changing headers order

--- holberton.h
+++ holberton.h
@@ -13,8 +13,8 @@ typedef struct specifiers
 	int (*f)(va_list args);
 } spc_dt;
 
-int _putchar(char c);
 int _printf(const char *format, ...);
+int _putchar(char c);
 
 int _print_a_char(va_list args);
 int _print_a_string(va_list args);
@@ -23,8 +23,8 @@ int _print_spec(char format, va_list args);
 int _print_invalid_spec(char prev_format, char format, int count);
 int _print_a_integer(va_list args);
 void _recursion_integer(int a);
-int _print_int_binary(va_list args);
-void _recursion_int_binary(int a);
+int _print_int_binary(va_list args)
+void _recursion_int_binary(int a)
 /*
 *  int _print_int_octal(va_list ap);
 * int _recursion_int__octal(unsigned int num, int *count);
