1372a030c34532823e9f9965e5bfb13394fb60ce
Renzo <renzin@gmail.com>
2021-03-15 14:35:25 +0000

    adding octal headers

--- holberton.h
+++ holberton.h
@@ -15,6 +15,7 @@ typedef struct specifiers
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+
 int _print_a_char(va_list args);
 int _print_a_string(va_list args);
 int _print_format(const char *format, va_list args);
@@ -23,10 +24,13 @@ int _print_invalid_spec(char prev_format, char format, int count);
 int _print_a_integer(va_list args);
 void _recursion_integer(int a);
 int _print_int_binary(va_list args);
+void _recursion_int_binary(int a);
+int _print_int_octal(va_list ap);
+int _recursion_int__octal(unsigned int num, int *count);
 int _print_reverse(va_list arg);
 int _print_rot(va_list arg);
 int _print_String(va_list arg);
-void _recursion_int_binary(int a);
+
 int _validate_char(char _type);
 
 #endif /* _HOLBERTON */
