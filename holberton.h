d364125563a097ae92511faa821a5539e6619067
Renzo <renzin@gmail.com>
2021-03-15 14:46:52 +0000

    adding unsigned headers

--- holberton.h
+++ holberton.h
@@ -27,6 +27,9 @@ int _print_int_binary(va_list args);
 void _recursion_int_binary(int a);
 int _print_int_octal(va_list ap);
 int _recursion_int__octal(unsigned int num, int *count);
+int _print_int_unsigned(va_list ap);
+int _recursion_int_unsigned(unsigned int num, int *count);
+
 int _print_reverse(va_list arg);
 int _print_rot(va_list arg);
 int _print_String(va_list arg);
