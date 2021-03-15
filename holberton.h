85e8e056085dc38ea5b401bb1b2e0f44275c2041
joseheco <jferhc@gmail.com>
2021-03-15 18:22:09 -0500

    comment2linesup

--- holberton.h
+++ holberton.h
@@ -23,7 +23,8 @@ int _print_spec(char format, va_list args);
 int _print_invalid_spec(char prev_format, char format, int count);
 int _print_a_integer(va_list args);
 void _recursion_integer(int a);
-/**int _print_int_binary(va_list args);
+/*
+* int _print_int_binary(va_list args);
 *void _recursion_int_binary(int a);
 *  int _print_int_octal(va_list ap);
 * int _recursion_int__octal(unsigned int num, int *count);
