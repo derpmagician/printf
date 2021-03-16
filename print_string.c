175982f07030541d85d4e5aefaab4b7f92209f20
joseheco <jferhc@gmail.com>
2021-03-16 13:40:36 -0500

    Fixing String

--- print_string.c
+++ print_string.c
@@ -11,10 +11,21 @@ int _print_String(va_list arg)
 {
 
 char *str = va_arg(arg, char *);
-int i = 0;
+int c, i = 0;
 if (str == NULL)
 str = "(null)";
-for (; str[i]; i++)
-_putchar(str[i]);
+for (c = 0; str[c]; c++)
+{
+if (str[c] < 32 || str[c] >= 127)
+{
+_putchar(92);
+_putchar(120);
+}
+else 
+{
+_putchar(str[c]);
+i++;
+}
+}
 return (i);
 }
