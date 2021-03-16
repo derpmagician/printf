7f78908078d9ec71e2110446e8acb275c6eec30d
Renzo <renzin@gmail.com>
2021-03-16 00:38:26 +0000

    removing b format

--- print_format.c
+++ print_format.c
@@ -63,7 +63,6 @@ int _print_spec(char format, va_list args)
 		{"s", _print_a_string},
 		{"d", _print_a_integer},
 		{"i", _print_a_integer},
-		{"b", _print_int_binary},
 		{"R", _print_rot},
 		/*{"r", _print_reverse},*/
 		/*{"p", _print_address}, need work*/
@@ -116,7 +115,7 @@ int _print_invalid_spec(char prev_format, char format, int count)
   */
 int _validate_char(char _type)
 {
-	char _types[] = {'c', 's', 'd', 'i', 'b', 'R', '%'};
+	char _types[] = {'c', 's', 'd', 'i', 'R', '%'};
 	int i = 0;
 
 	while (_types[i])
